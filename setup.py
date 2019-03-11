dfccbeffcc5fb015ad8f0b5599aff6b35e369a7c
Enrique Tuya <enriquetuya@gmail.com>
2019-03-11 18:37:24 -0300

    See if this fixes the issue.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ DEPENDENCIES = [
     "newlinejson",
     "python-stdnum",
     "pipe-tools==0.1.7",
-    "gpsdio-segment==0.11.1",
+    "gpsdio-segment==0.11",
     "shipdataprocess==0.5.0",
     "jinja2-cli",
 ]
